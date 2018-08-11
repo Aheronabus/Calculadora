838b7d8b2a2e0f43adcb92abc27098388c6f547f
Gaos <ccubillos650@gmail.com>
2018-08-10 21:25:29 -0500

    Se han añadido las operaciones: Restar, Multiplicar y Dividir

--- Scripts/script2.js
+++ Scripts/script2.js
@@ -15,4 +15,45 @@ function sumar(frm){
 }
 
 
+function añadirNodo(operacion,numeros){
+
+	//Determinacion operacion
+
+	if (operacion=="sumar") {
+		resultado = parseInt(numeros[0]) + parseInt(numeros[1]);
+	} else if (operacion=="restar") {
+		resultado = parseInt(numeros[0]) - parseInt(numeros[1]);
+	} else if (operacion=="multiplicar") {
+		resultado = parseInt(numeros[0]) * parseInt(numeros[1]);
+	} else if (operacion=="dividir") {
+		resultado = parseInt(numeros[0]) / parseInt(numeros[1]);	
+	}
+
+	//Creacion nodo resultante
+
+	var nodo = document.createElement("span");
+	var contenido = document.createTextNode(resultado);
+	nodo.appendChild(contenido);
+
+	//Reemplazar nodo previo
+
+	if (document.getElementById("resultado").childNodes[0] != null){
+		nodoFinal = document.getElementById("resultado").childNodes[0]	
+		document.getElementById("resultado").replaceChild(nodo,nodoFinal);
+	} else{
+		document.getElementById("resultado").appendChild(nodo);
+	}
+
+}
+
+
+function operar(frm){
+	
+	//Obtencion Parametros 
+
+	var operacion = [frm.operaciones.value];
+	var numeros = [frm.num1.value,frm.num2.value];
+
+	añadirNodo(operacion,numeros);
+}
 
