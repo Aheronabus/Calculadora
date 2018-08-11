f3a912f429958de1dea115f0b4d89df6e14df443
Gaos <ccubillos650@gmail.com>
2018-08-10 21:33:31 -0500

    Se limpio el codigo

--- Scripts/script2.js
+++ Scripts/script2.js
@@ -1,20 +1,3 @@
-function sumar(frm){
-	var parametros = [frm.num1.value,frm.num2.value];
-	var resultado = parseInt(parametros[0]) + parseInt(parametros[1]);
-	var span = document.createElement("span");
-	var resultadoTexto = document.createTextNode(resultado);
-	span.appendChild(resultadoTexto);	
-	if (document.getElementById("resultado").childNodes[0] != null){
-		nodo = document.getElementById("resultado").childNodes[0]	
-		document.getElementById("resultado").replaceChild(span,nodo);
-	} else{
-		document.getElementById("resultado").appendChild(span);
-	}
-	
-	
-}
-
-
 function añadirNodo(operacion,numeros){
 
 	//Determinacion operacion
