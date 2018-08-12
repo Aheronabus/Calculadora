2d7a1074c588b018c39a5580b6b7c6987e8909b8
Gaos <ccubillos650@gmail.com>
2018-08-12 10:46:21 -0500

    Se ha dotado de funcionalidad a la calculadora. Puede Sumar, Restar, Dividir y Multiplicar. Ademas de imprimir todo en pantalla. Falta agregar la jerarquizacion de operadores.

--- Scripts/script2.js
+++ Scripts/script2.js
@@ -1,3 +1,4 @@
+/*
 	function añadirNodo(operacion,numeros){
 
 	//Determinacion operacion
@@ -39,4 +40,149 @@ function operar(frm){
 
 	añadirNodo(operacion,numeros);
 }
+*/
 
+//Declaracion variables globales 
+
+var digitos = [];
+var numerosCompletos = [];
+var operandos = [];
+var resultado = 0;
+
+function submit(value){
+	if (!(isNaN(value))) {
+		digitos.push(value);
+		console.log(value);
+		crearNodo("span","pantallaDigitos",value)
+		
+	} else {
+		switch(value){
+
+			//Comprobar operador a agregar 
+
+			case '+':
+				 concatenar(digitos);
+				 operandos.push("+");
+				 digitos = [];
+				 console.log(numerosCompletos);
+				 crearNodo("span","pantallaDigitos","+")
+				 break;
+			case '-':
+				 concatenar(digitos);
+				 operandos.push("-");
+				 digitos = [];
+				 console.log(numerosCompletos);
+				 crearNodo("span","pantallaDigitos","-")
+				 break;
+			case '*':
+				 concatenar(digitos);
+				 operandos.push("*");
+				 digitos = [];
+				 console.log(numerosCompletos);
+				 crearNodo("span","pantallaDigitos","*")
+				 break;
+			case '/':
+				 concatenar(digitos);
+				 operandos.push("/");
+				 digitos = [];
+				 console.log(numerosCompletos);
+				 crearNodo("span","pantallaDigitos","/")
+				 break;
+			case '=':
+				concatenar(digitos);
+				digitos = [];
+				for (var i = 0; i < operandos.length; i++) {
+					
+					if (resultado == 0) {
+						switch(operandos[i]){
+							case '+':
+								resultado = parseFloat(numerosCompletos[i]) + parseFloat(numerosCompletos[i+1]);
+								break;
+							case '-':
+								resultado = parseFloat(numerosCompletos[i]) - parseFloat(numerosCompletos[i+1]);
+								break;
+							case '*':
+								resultado = parseFloat(numerosCompletos[i]) * parseFloat(numerosCompletos[i+1]);
+								break;
+							case '/':
+								resultado = parseFloat(numerosCompletos[i]) / parseFloat(numerosCompletos[i+1]);
+								break;
+						}	
+					}else{
+						switch(operandos[i]){
+							case '+':
+								resultado = resultado + parseFloat(numerosCompletos[i+1]);
+								break;
+							case '-':
+								resultado = resultado - parseFloat(numerosCompletos[i+1]);
+								break;
+							case '*':
+								resultado = resultado * parseFloat(numerosCompletos[i+1]);
+								break;
+							case '/':
+								resultado = resultado / parseFloat(numerosCompletos[i+1]);
+								break;
+						}
+					}					
+				}
+				console.log(resultado);
+				crearNodo("span","pantallaResultados",resultado)
+				resultado = 0;
+				digitos = [];
+				operandos = [];
+				numerosCompletos = [];
+				break;
+			case 'c':
+				console.clear();
+				operandos = [];
+				digitos = [];
+				numerosCompletos = [];
+				resultado = 0;
+				limpiarPantallaCalculadora("pantallaDigitos");
+				limpiarPantallaCalculadora("pantallaResultados");
+				break;
+			case '<':
+				digitos.pop();
+				borrarDigito("pantallaDigitos");
+				break;
+			case '.':
+				digitos.push(".");
+				crearNodo("span","pantallaDigitos",".")
+		}
+	}
+}
+
+function concatenar(array){
+	concatenacion = 0
+	if (array.length > 1) {
+		for (var i = 0; i < array.length - 1; i++) {
+		if (concatenacion == 0) {
+			concatenacion = String(array[i]) + String(array[i+1]);
+		}else{
+			concatenacion = concatenacion + String(array[i+1]);
+		}
+	}
+	numerosCompletos.push(concatenacion);	
+	}else{
+		numerosCompletos.push(array[0]);
+	}	
+}
+
+function crearNodo(typeNode,objective,node){
+	var nodo = document.createElement(typeNode);
+	var contenido = document.createTextNode(node);
+	nodo.appendChild(contenido);
+	document.getElementById(objective).appendChild(nodo);
+}
+
+function limpiarPantallaCalculadora(objective){
+	var objetivo = document.getElementById(objective);
+	while(objetivo.childNodes.length >= 1){
+		objetivo.removeChild(objetivo.firstChild);
+	}
+}
+
+function borrarDigito(objective){
+	var objetivo = document.getElementById(objective);
+	objetivo.removeChild(objetivo.lastChild);
+}
